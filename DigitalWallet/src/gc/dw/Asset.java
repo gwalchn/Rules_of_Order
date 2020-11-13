ae57604b1f903bcd631ddfca77c680ab4a966577
guocheng <guocheng@guochengdeair>
2020-11-13 16:25:01 +0800

    New file for Asset

---
+++ DigitalWallet/src/gc/dw/Asset.java
@@ -0,0 +1,9 @@
+// ©2020 Cheng GUO. All rights reserved. 
+package gc.dw;
+
+public interface Asset {
+	
+	public double getValue();
+	public String getName();
+	public String getDescription();
+}
--- DigitalWallet/src/gc/dw/Wallet.java
+++ DigitalWallet/src/gc/dw/Wallet.java
@@ -1,5 +1,6 @@
 package gc.dw;
 
 public interface Wallet {
-
+	
+	public void getAsset(int id);
 }
