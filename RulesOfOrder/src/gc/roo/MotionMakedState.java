6f4e04a12e0e266beebe2e55d1c2fdb1788c973e
guocheng <guocheng@192.168.1.91>
2021-04-28 15:43:10 +0800

    遗漏补提交

---
+++ RulesOfOrder/src/gc/roo/MotionMakedState.java
@@ -0,0 +1,46 @@
+package gc.roo;
+
+public class MotionMakedState implements MotionState {
+	private Motion motion;
+
+	public MotionMakedState(Motion motion) {
+		this.motion = motion;
+	}
+
+	@Override
+	public void makeMotion() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void secondMotion() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void StateQuestionOnMotion() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void debate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void putTheQuestion() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void announceTheResult() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
